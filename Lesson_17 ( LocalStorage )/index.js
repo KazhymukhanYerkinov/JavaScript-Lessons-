1a7289a635e846f7b41350ed235255e93d8c29ed
KazhymukhanYerkinov <61079138+KazhymukhanYerkinov@users.noreply.github.com>
2020-12-30 20:55:12 +0600

    '...'

---
+++ Lesson_17 ( LocalStorage )/index.js
@@ -0,0 +1,26 @@
+const myNumber = 42;
+
+localStorage.removeItem('number');
+
+console.log(localStorage.getItem('number'));
+localStorage.setItem('number', myNumber.toString());
+console.log(localStorage.getItem('number'));
+
+localStorage.clear();
+
+const object = {
+	name: 'Max',
+	age: 20
+}
+localStorage.setItem('person', JSON.stringify(object))
+
+const raw = localStorage.getItem('person');
+const person = JSON.parse(raw);
+raw.name = 'Kazhymukhan'
+
+console.log(person);
+
+
+window.addEventListener('storage', event => {
+	console.log(event)
+})
